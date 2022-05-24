dc7863de51c772f4827dbc7533d30017723e9958
kineticvenom <kalebvarnes@yahoo.com>
2022-05-24 08:35:08 -1000

    Code Added

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,26 @@
-function deafGrandma() {
+function deafGrandma(input) {
+const goodbyeCnt = 0
+   
+if (input === null) {
+        console.log(`WHAT?!`);
+    }
+    if (input === /g[a-z]/){
+        console.log(`SPEAK UP, KID!`)
+    }
+    if (input === `GOODBYE!`){
+        goodbyeCnt++;
+        if (goodbyeCnt === 1) {
+console.log(`LEAVING SO SOON?`)
+        } else if (goodbyeCnt === 2) {
+            console.log(`LATER, SKATER!`)
+            return
+        }
+    }
+    else if (input === /g[A-Z]/){
+        console.log(`NO, NOT SINCE 1946!`)
+    }
+    
 
 }
 
-deafGrandma();
+console.log(deafGrandma(`hi, grandma`));
