83c11bc2af53e3b112df16e925cb10e0ba7e8229
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 17:17:25 -1000

    Code added

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,26 +1,25 @@
-function deafGrandma(input) {
-const goodbyeCnt = 0
-   
-if (input === null) {
-        console.log(`WHAT?!`);
-    }
-    if (input === /g[a-z]/){
-        console.log(`SPEAK UP, KID!`)
-    }
-    if (input === `GOODBYE!`){
-        goodbyeCnt++;
-        if (goodbyeCnt === 1) {
-console.log(`LEAVING SO SOON?`)
-        } else if (goodbyeCnt === 2) {
-            console.log(`LATER, SKATER!`)
-            return
+function deafGrandma() {
+    const goodbyeCnt = 0
+    let input = window.prompt('HEY KID!')   
+    while (goodbyeCnt < 2) {
+        if (input === null) {
+            window.alert(`WHAT?!`);
         }
+        else if (input === /g[a-z]/){
+            window.alert(`SPEAK UP, KID!`)
+        }
+        else if (input === /g[A-Z]/){
+            window.alert(`NO, NOT SINCE 1946!`)
+        }
+        else if (input === `GOODBYE!`){
+            goodbyeCnt++;
+        }
+        else if (goodbyeCnt === 1) {
+            window.alert(`LEAVING SO SOON?`)
+        } 
+        else if (goodbyeCnt === 2) {
+            window.alert(`LATER, SKATER!`)  
+        }  
     }
-    else if (input === /g[A-Z]/){
-        console.log(`NO, NOT SINCE 1946!`)
-    }
-    
-
 }
-
-console.log(deafGrandma(`hi, grandma`));
+deafGrandma()
