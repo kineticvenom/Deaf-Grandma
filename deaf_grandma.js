37baa343ae54f9cc5f5e994ace747e65007a2a61
kineticvenom <kalebvarnes@yahoo.com>
2022-05-27 05:19:26 -1000

    not working in html

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,25 +1,28 @@
-function deafGrandma() {
+// function deafGrandma() {
     const goodbyeCnt = 0
-    let input = window.prompt('HEY KID!')   
+       
     while (goodbyeCnt < 2) {
-        if (input === null) {
-            window.alert(`WHAT?!`);
-        }
-        else if (input === /g[a-z]/){
-            window.alert(`SPEAK UP, KID!`)
-        }
-        else if (input === /g[A-Z]/){
-            window.alert(`NO, NOT SINCE 1946!`)
-        }
-        else if (input === `GOODBYE!`){
+        let input = prompt()
+
+        if (input === "") {
+            alert(`WHAT?!`);
+        }else if (input === `GOODBYE!`){
             goodbyeCnt++;
+        
+            if (goodbyeCnt === 1) {
+                alert(`LEAVING SO SOON?`)
+            } 
+            else if (goodbyeCnt === 2) {
+                alert(`LATER, SKATER!`)  
+            }  
+        }    
+        else if (input == /g[a-z]/){
+            alert(`SPEAK UP, KID!`)
         }
-        else if (goodbyeCnt === 1) {
-            window.alert(`LEAVING SO SOON?`)
-        } 
-        else if (goodbyeCnt === 2) {
-            window.alert(`LATER, SKATER!`)  
-        }  
+        else if (input == /g[A-Z]/){
+            alert(`NO, NOT SINCE 1946!`)
+        }
+        
     }
-}
-deafGrandma()
+// }
+// deafGrandma()
