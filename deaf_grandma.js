45aaeb4d9ebfbd61e0594657bd5dce5c92746746
kineticvenom <kalebvarnes@yahoo.com>
2022-05-27 05:49:36 -1000

    REGEX Works!

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,26 +1,26 @@
 // function deafGrandma() {
-    const goodbyeCnt = 0
+    let goodbyeCnt = 0
        
     while (goodbyeCnt < 2) {
-        let input = prompt()
+        let input = window.prompt()
 
         if (input === "") {
-            alert(`WHAT?!`);
-        }else if (input === `GOODBYE!`){
+            window.alert(`WHAT?!`);
+        }else if (input === `GOODBYE!`) {
             goodbyeCnt++;
         
             if (goodbyeCnt === 1) {
-                alert(`LEAVING SO SOON?`)
+                window.alert(`LEAVING SO SOON?`)
             } 
             else if (goodbyeCnt === 2) {
-                alert(`LATER, SKATER!`)  
+                window.alert(`LATER, SKATER!`)  
             }  
         }    
-        else if (input == /g[a-z]/){
-            alert(`SPEAK UP, KID!`)
+        else if (input.match(/^[a-z\s]/)){ 
+            window.alert(`SPEAK UP, KID!`)
         }
-        else if (input == /g[A-Z]/){
-            alert(`NO, NOT SINCE 1946!`)
+        else if (/^[A-Z\s]/.test(input)){
+            window.alert(`NO, NOT SINCE 1946!`)
         }
         
     }
